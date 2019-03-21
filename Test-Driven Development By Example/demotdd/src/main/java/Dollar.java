9b2aa338695740fceaff1a32d6502d624ff708be
DevGGyu <dev.ggyu@gmail.com>
2019-03-21 19:31:57 +0900

    Test Driven Development 1일차 진행
    Signed-off-by: DevGGyu <dev.ggyu@gmail.com>

---
+++ Test-Driven Development By Example/demotdd/src/main/java/Dollar.java
@@ -0,0 +1,17 @@
+class Dollar {
+    int amount;
+
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Dollar dollar = (Dollar) obj;
+        return amount == dollar.amount;
+    }
+}
