1c44eb3fcedfab0f6404a03b61ba9854893718ae
harry <dev.ggyu@gmail.com>
2019-03-23 22:04:39 +0900

    ch 09 ~ ch 11
    Signed-off-by: harry <dev.ggyu@gmail.com>

--- Test-Driven Development By Example/demotdd/src/main/java/Money.java
+++ Test-Driven Development By Example/demotdd/src/main/java/Money.java
@@ -1,22 +1,38 @@
-abstract class Money {
+class Money {
 
     protected int amount;
+    protected String currency;
 
-    abstract Money times(int multiplier);
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    String currency() {
+        return currency;
+    }
 
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Money(amount, "CHF");
     }
 
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
         return amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency().equals(money.currency());
     }
 
+    @Override
+    public String toString() {
+        return amount + " " + currency;
+    }
 }
