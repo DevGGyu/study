d6cd90513d1d41207a6ded093a72710fa77e3403
DevGGyu <dev.ggyu@gmail.com>
2019-03-23 18:05:09 +0900

    ch 07 ~ ch 08
    Signed-off-by: DevGGyu <dev.ggyu@gmail.com>

--- Test-Driven Development By Example/demotdd/src/main/java/Money.java
+++ Test-Driven Development By Example/demotdd/src/main/java/Money.java
@@ -1,11 +1,22 @@
-class Money {
+abstract class Money {
 
     protected int amount;
+
+    abstract Money times(int multiplier);
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
-        return amount == money.amount;
+        return amount == money.amount
+                && getClass().equals(money.getClass());
     }
 
 }
