37c042ecf9e3c21d636491ef10faa7ab7939e532
dev.ggyu <dev.ggyu@gmail.com>
2023-01-08 20:20:00 +0900

    ch20
    Signed-off-by: dev.ggyu <dev.ggyu@gmail.com>

--- study(2023)/StateExample/app/src/main/java/com/ggyu/stateexample/MainActivity.kt
+++ study(2023)/StateExample/app/src/main/java/com/ggyu/stateexample/MainActivity.kt
@@ -3,11 +3,14 @@ package com.ggyu.stateexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ggyu.stateexample.ui.theme.StateExampleTheme
@@ -36,17 +39,37 @@ fun DemoScreen() {
 
 @Composable
 fun MyTextField() {
-    var textState by remember { mutableStateOf("") }
+    var textState by rememberSaveable { mutableStateOf("") }
 
     val onTextChange = { text: String -> textState = text }
 
     TextField(value = textState, onValueChange = onTextChange)
 }
 
-@Preview(showBackground = true)
+@Composable
+fun FunctionA() {
+    var switchState by remember { mutableStateOf(true) }
+
+    val onSwitchChange = { value: Boolean -> switchState = value }
+
+    FunctionB(
+        switchState = switchState,
+        onSwitchChange = onSwitchChange
+    )
+}
+
+@Composable
+fun FunctionB(switchState: Boolean, onSwitchChange: (Boolean) -> Unit) {
+    Switch(checked = switchState, onCheckedChange = onSwitchChange)
+}
+
+@Preview(showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     StateExampleTheme {
-        DemoScreen()
+        Column {
+            DemoScreen()
+            FunctionA()
+        }
     }
 }
