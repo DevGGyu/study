405002a5e6d60726effbaafa8b67e6fb01169c66
dev.ggyu <dev.ggyu@gmail.com>
2023-02-02 23:14:13 +0900

    ch34.3
    Signed-off-by: dev.ggyu <dev.ggyu@gmail.com>

--- study(2023)/jpub-jetpack-compose/ListDemo/app/src/main/java/com/example/listdemo/MainActivity.kt
+++ study(2023)/jpub-jetpack-compose/ListDemo/app/src/main/java/com/example/listdemo/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -39,7 +41,10 @@ fun MainScreen() {
 
 @Composable
 fun ColumnList() {
-    Column {
+
+    val scrollState = rememberScrollState()
+
+    Column(Modifier.verticalScroll(scrollState)) {
         repeat(500) {
             Text(
                 "List Item $it",
