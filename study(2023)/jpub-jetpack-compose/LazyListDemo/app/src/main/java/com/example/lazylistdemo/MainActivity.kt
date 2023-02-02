979e3a12a8f24f48a4ba4b8742576f399efdf820
dev.ggyu <dev.ggyu@gmail.com>
2023-02-02 23:41:43 +0900

    ch35.4
    Signed-off-by: dev.ggyu <dev.ggyu@gmail.com>

--- study(2023)/jpub-jetpack-compose/LazyListDemo/app/src/main/java/com/example/lazylistdemo/MainActivity.kt
+++ study(2023)/jpub-jetpack-compose/LazyListDemo/app/src/main/java/com/example/lazylistdemo/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.lazylistdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.lazylistdemo.ui.theme.LazyListDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,8 +40,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(itemArray: Array<out String>) {
+fun ImageLoader(item: String) {
+    val url =
+        "https://www.ebookfrenzy.com/book_examples/car_logos/${item.substringBefore(" ")}_logo.png"
 
+    Image(
+        painter = rememberImagePainter(url),
+        contentDescription = "car image",
+        contentScale = ContentScale.Fit,
+        modifier = Modifier.size(75.dp)
+    )
+}
+
+@Composable
+fun MainScreen(itemArray: Array<out String>) {
+    ImageLoader("Plymouth GTX")
 }
 
 @Preview(showBackground = true)
