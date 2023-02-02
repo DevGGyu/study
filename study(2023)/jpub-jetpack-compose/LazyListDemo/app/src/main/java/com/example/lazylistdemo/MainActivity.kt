dce166b157ad65f87dd102067341a36888c386ef
dev.ggyu <dev.ggyu@gmail.com>
2023-02-02 23:51:26 +0900

    ch35.8
    Signed-off-by: dev.ggyu <dev.ggyu@gmail.com>

--- study(2023)/jpub-jetpack-compose/LazyListDemo/app/src/main/java/com/example/lazylistdemo/MainActivity.kt
+++ study(2023)/jpub-jetpack-compose/LazyListDemo/app/src/main/java/com/example/lazylistdemo/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.lazylistdemo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -16,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
@@ -45,11 +48,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyListItem(item: String) {
+fun MyListItem(item: String, onItemClick: (String) -> Unit) {
     Card(
         modifier = Modifier
             .padding(8.dp)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .clickable { onItemClick(item) },
         shape = RoundedCornerShape(10.dp),
         elevation = 5.dp
     ) {
@@ -81,9 +85,16 @@ fun ImageLoader(item: String) {
 
 @Composable
 fun MainScreen(itemArray: Array<out String>) {
+
+    val context = LocalContext.current
+
+    val onListItemClick = { text: String ->
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
+
     LazyColumn {
         items(itemArray) { model ->
-            MyListItem(model)
+            MyListItem(model, onItemClick = onListItemClick)
         }
     }
 }
